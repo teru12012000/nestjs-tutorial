3da5ca0d9ef8f504f2618012944f5bcab88055b2
teru12012000 <sion0012@outlook.jp>
2022-12-19 17:14:40 +0900

    studied

--- Record/MEMO.md
+++ Record/MEMO.md
@@ -4,8 +4,13 @@
 
 - プロジェクト作成の仕方
 - ルーティング
+- リソース
+- ルートワイルドカード
+- ステータスコードとヘッダー
 
 ## わかったこと
 
 - コントローラを作成する場合
   `$nest g controller cats`
+- `get post put delete`は`@Get @Post @Put @Delete`と宣言して関数を書く
+- `@Get()`以外の確認は基本的に Postman で行う
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  //Req,
+  //HttpCode,
+  //Header,
+  //Redirect,
+} from '@nestjs/common';
+//import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
-  @Get()
+  @Post('add')
+  //@HttpCode(201)
+  //@Header('Cache-Control', 'none')
+  create(): string {
+    return 'This action adds a new cat';
+  }
+  @Get('find')
+  //@Redirect('https://nestjs.com', 301)
   findAll(): string {
     return 'This action returns all cats';
   }
